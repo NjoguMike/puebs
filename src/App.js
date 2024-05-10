83a3bcc8b6399d8cb82c912b9586588cc9b49283
Michael Njogu <michaelfebzen.mb@gmail.com>
2024-05-10 17:02:23 +0300

    Sorted the server watch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import '../src/Styling/App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div> This is a new document
+    <div>
+      <Routes>
+        <Route index element={<Home/>}/>
+      </Routes>
     </div>
+
   );
 }
 
