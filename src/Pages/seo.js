811a63c6501b9cc4277130c825243c8a09a191ef
Michael Njogu <michaelfebzen.mb@gmail.com>
2024-05-11 14:53:43 +0300

    Components

--- src/Pages/seo.js
+++ src/Pages/seo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ServiceFooter from '../Components/services_footer'
+import ServicesSection from '../Components/ServicesSection'
 
 function Seo() {
   return (
@@ -9,6 +10,7 @@ function Seo() {
             <p>If you have not yet been introduced to what SEO marketing is, then allow us to show you the path to your future in dominating your local market. Search Engine Optimization is the way that our team makes sure that your website, social media presence, advertising and all other online presence is geared towards getting eyes on your business. The internet is a great tool for business, but the algorithms for search engines like Google are complicated. So, many people pay for a website to be designed, but when no one can see your beautiful website, it really doesn’t make an impact on your bottom line.</p>
             <p>Instead of focusing on just the visual aspects, our team of skilled SEO marketing gurus are able to design your website in a way that boosts your ranking on Google for relevant search terms. Getting eyes on your website is the very first step in making it possible for you to secure more sales. From our time researching and practicing the best SEO methods for web design, advertising, and content creation, we know exactly what Google and other influential search engines want. If you are looking for an opportunity to connect with the audience that is out there waiting for your products and services, then SEO marketing and web design is the way to go.</p>
         </div>
+        <ServicesSection />
         <ServiceFooter />
     </div>
   )
