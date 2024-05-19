bd2c3e13a661c3b681b0709603869300f1b2e858
Michael Njogu <michaelfebzen.mb@gmail.com>
2024-05-19 23:00:22 +0300

    Footer design

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import logo from '../Assets/puebloico.jpg'
 
 function Footer() {
   return (
-    <div>
-        <div>
-            <h2>CONTACT US</h2>
-            <p>PUELBO COLORADO | 8AM-5PM <br/> BADASSMARKETERS@GMAIL.COM </p>
-        </div>
-        <div>
-            <h2>LET’S CONNECT</h2>
-        </div>
-        <div>
-            <h2>NAVIGATION</h2>
-            <ul className='nav'>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/about'}>About Us</Link>
-                <Link to={'/seo'}>Our Seo</Link>
-                <Link to={'/privacy'}>Privacy</Link>
-            </ul>
+    <div className='footer'>
+        <h2>LET’S CONNECT</h2>
+        <div className='footer-container'>
+            <div>
+                <img src={logo} alt='logo'/>
+                <h3>CONTACT US</h3>
+                <p>Pueblo Colorado | 8AM-5PM <br/><br/> badassmarketers@gmail.com</p>
+            </div>
+            <div className='navigation'>
+                <h3>NAVIGATION</h3>
+                <hr/>
+                <ul className='nav nav-footer'>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/about'}>About Us</Link>
+                    <Link to={'/seo'}>Our Seo</Link>
+                    <Link to={'/privacy'}>Privacy</Link>
+                </ul>
+            </div>
         </div>
     </div>
   )
