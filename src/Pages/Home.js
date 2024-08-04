bf067046882211145d15644e07824785ccb9a2a5
Michael Njogu <michaelfebzen.mb@gmail.com>
2024-08-04 21:26:40 +0300

    new css rules

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ import profile from '../Assets/profile.jpg'
 import profile_2 from '../Assets/profile_2.jpg'
 import profile_3 from '../Assets/profile_3.jpg'
 import Slider from 'infinite-react-carousel';
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaArrowRight } from 'react-icons/fa'
 
 function Home() {
     const testimonialSettings = {
@@ -33,8 +33,11 @@ function Home() {
         <section className='intro'>
             <div className='intro_card'>
                 <h1>What Digital Daktariz brings You!</h1>
-                <p>All of the search engines that people regularly use are using algorithms to determine which websites are most relevant to the search term that they are using. What our team does, is make sure that we have a complete understanding of how those algorithms work, and then we use your website and social media accounts to make sure that your business is the most relevant place for Google to send people for search terms that relate to your field. When people google to find the “best Mexican restaurant” there may actually be several around them that they don’t notice right away in the search results. With our strategy, we would make sure that restaurant is able to be seen right there on the first page of results, so that you do not miss out on any potential customers. If you are in the process of building your audience and trying to take your business to the next level, then you have to be excited about the opportunities available to market your business on the internet. Every single project we launch here at Digital Daktariz marketing begins with a thorough, often intense planning process. We involve as many of our in-house experts as needed to ensure that your plan is sound and successful.</p>
-                <button>Read More</button>
+                <p>All of the search engines that people regularly use are using algorithms to determine which websites are most relevant to the search term that they are using. What our team does, is make sure that we have a complete understanding of how those algorithms work, and then we use your website and social media accounts to make sure that your business is the most relevant place for Google to send people for search terms that relate to your field. If you are in the process of building your audience and trying to take your business to the next level, then you have to be excited about the opportunities available to market your business on the internet. Every single project we launch here at Digital Daktariz marketing begins with a thorough, often intense planning process. We involve as many of our in-house experts as needed to ensure that your plan is sound and successful.</p>
+                <button className='readmorebtn'>
+                    <span>About Us </span>
+                    <span className='icon'><FaArrowRight /></span>
+                </button>
             </div>
             <div className='intro_image'>
                 <img src={landing_image2} alt='landing-view' className='jumbo_image' loading='lazy'/>
@@ -44,24 +47,20 @@ function Home() {
             <div className='card'>
                 <h3>SEO</h3>
                 <p>Looking for an opportunity to connect with the right audience?, then SEO marketing is the way to go.</p>
-                <button>READ MORE</button>
             </div>
             <div className='card'>
                 <h3>WEB DESIGN</h3>
                 <p>Having a perfect pitch won’t do you any good if it isn’t pitched at the right time or place. We ensure every shilling spent contributes towards your ROI. </p>
-                <button>READ MORE</button>
             </div>
             <div className='card'>
                 <h3>SOCIAL MEDIA Ads</h3>
                 <p>Want a social media campaign that knocks people’s socks off? Well, so do we, and we know just what needs to happen to make that a reality. </p>
-                <button>READ MORE</button>
             </div>
         </section>
         <section className='intro'>
             <div className='intro_card'>
                 <h1>SEO Services in Digital Daktariz</h1>
-                <p>If you have spent time looking into your marketing strategy in 2018 then you might have heard SEO thrown around a lot by peers as well as seen it online when looking for marketing services. SEO simply stands for <b>Search Engine Optimization</b>. Think about it, when you are looking for a restaurant, a plumber, a tow truck, or really just about any information, where do you go? Google is the juggernaut but even people that don’t use google use a search engine of some kind to get information. Search engine optimization is the scientific process that we use to boost your presence on search engine results in the area of your business.</p>
-                <p>All of the search engines that people regularly use are using algorithms to determine which websites are most relevant to the search term that they are using. What our team does, is make sure that we have a complete understanding of how those algorithms work, and then we use your website and social media accounts to make sure that your business is the most relevant place for Google to send people for search terms that relate to your field. When people google to find the “best Mexican restaurant” there may actually be several around them that they don’t notice right away in the search results. With our strategy, we would make sure that restaurant is able to be seen right there on the first page of results, so that you do not miss out on any potential customers.</p>
+                <p>If you spent time looking into your marketing strategy in 2018 then you might have heard the term SEO thrown around a lot by peers as well as seen it online when looking for marketing services. SEO simply stands for <b>Search Engine Optimization</b>. Think about it, when you are looking for a restaurant, a plumber, a tow truck, or really just about anything, where do you go? Google, Google is the juggernaut but even people that don’t use google use a search engine of some kind to get information. Search engine optimization is the process that we use to boost your presence on search engine results in the area of your business.</p>
             </div>
             <div className='intro_image'>
                 <img src={services_image} alt='landing-view' className='jumbo_image'/>
